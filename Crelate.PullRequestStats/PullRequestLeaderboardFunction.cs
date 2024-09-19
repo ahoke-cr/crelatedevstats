895ce1d1e853cbff8ddc31a28497e64b7e7dbc1d
Andrew Hoke <ahoke@crelate.com>
2024-09-19 11:23:39 -0700

    Add project files.

---
+++ Crelate.PullRequestStats/PullRequestLeaderboardFunction.cs
@@ -0,0 +1,14 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Crelate.PullRequestStats;
+
+public class PullRequestLeaderboardFunction
+{
+	[FunctionName("Function1")]
+	public void Run([TimerTrigger("0 0 1 * * Mon")]TimerInfo myTimer, ILogger log)
+	{
+		log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+	}
+}
