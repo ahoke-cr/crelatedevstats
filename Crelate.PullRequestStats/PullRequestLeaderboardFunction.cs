4bafea60ced712051420d9ac1d3bf9cb541ac51e
Andrew Hoke <ahoke@crelate.com>
2024-09-19 11:24:39 -0700

    Rename function

--- Crelate.PullRequestStats/PullRequestLeaderboardFunction.cs
+++ Crelate.PullRequestStats/PullRequestLeaderboardFunction.cs
@@ -6,7 +6,7 @@ namespace Crelate.PullRequestStats;
 
 public class PullRequestLeaderboardFunction
 {
-	[FunctionName("Function1")]
+	[FunctionName("PullRequestLeaderboardFunction")]
 	public void Run([TimerTrigger("0 0 1 * * Mon")]TimerInfo myTimer, ILogger log)
 	{
 		log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
